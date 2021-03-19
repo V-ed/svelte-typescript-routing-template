11b14972b4b7f202139fcaf5f8d5c1fef0fd0e54
V-ed <guillaumemarcoux@gmail.com>
2021-03-19 11:11:32 -0400

    Remove console logging on vite defineConfig call

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,10 @@ import { defineConfig, loadEnv } from 'vite';
 import WindiCSS from 'vite-plugin-windicss';
 
 export default defineConfig(({ mode }) => {
-	console.log(mode);
-
 	Object.assign(process.env, loadEnv(mode, process.cwd()));
 
 	const isProduction = mode === 'production';
 
-	console.log('env port : ', process.env.PORT);
 	const port: number = parseInt(process.env.PORT) || 5000;
 
 	const targets = ['es2020'];
