a7536f515948fc0bca1c65b4f9bced973f28a077
V-ed <guillaumemarcoux@gmail.com>
2021-04-11 18:31:49 -0400

    Tweak vite config pwa options to only define manifest

--- vite.config.ts
+++ vite.config.ts
@@ -6,33 +6,31 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 const DEFAULT_PORT = 5000;
 
-export const pwaOptions: Partial<VitePWAOptions> = {
-	manifest: {
-		name: 'Svelte Typescript Routing Template',
-		short_name: 'Svelte Typescript Routing Template',
-		icons: [
-			{
-				src: '/icons/pwa/icon-192x192.png',
-				sizes: '192x192',
-				type: 'image/png',
-			},
-			{
-				src: '/icons/pwa/icon-256x256.png',
-				sizes: '256x256',
-				type: 'image/png',
-			},
-			{
-				src: '/icons/pwa/icon-384x384.png',
-				sizes: '384x384',
-				type: 'image/png',
-			},
-			{
-				src: '/icons/pwa/icon-512x512.png',
-				sizes: '512x512',
-				type: 'image/png',
-			},
-		],
-	},
+export const manifestOptions: VitePWAOptions['manifest'] = {
+	name: 'Svelte Typescript Routing Template',
+	short_name: 'Svelte Typescript Routing Template',
+	icons: [
+		{
+			src: '/icons/pwa/icon-192x192.png',
+			sizes: '192x192',
+			type: 'image/png',
+		},
+		{
+			src: '/icons/pwa/icon-256x256.png',
+			sizes: '256x256',
+			type: 'image/png',
+		},
+		{
+			src: '/icons/pwa/icon-384x384.png',
+			sizes: '384x384',
+			type: 'image/png',
+		},
+		{
+			src: '/icons/pwa/icon-512x512.png',
+			sizes: '512x512',
+			type: 'image/png',
+		},
+	],
 };
 
 export default defineConfig(({ mode }) => {
@@ -63,7 +61,7 @@ export default defineConfig(({ mode }) => {
 		plugins: [
 			VitePWA({
 				minify: !isProduction,
-				...pwaOptions,
+				manifest: manifestOptions,
 			}),
 			VitePluginWindicss(),
 			VitePluginSvelte({
