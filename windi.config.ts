c6a3804b8cc72ff37fafe62450a39c24bc2c7fc4
V-ed <guillaumemarcoux@gmail.com>
2021-04-23 12:33:27 -0400

    Disable windicss preflight for materialify

--- windi.config.ts
+++ windi.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite-plugin-windicss';
 
 export default defineConfig({
 	darkMode: 'media',
-	important: true,
+	preflight: false,
 	theme: {
 		container: {
 			center: true,
