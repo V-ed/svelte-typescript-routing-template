8dd3fc6ed67c0700ffb84c172176f2850c29167a
V-ed <guillaumemarcoux@gmail.com>
2021-04-29 19:01:49 -0400

    Tweak toggleable parameter to be more verbose

--- src/stores/toggleable.ts
+++ src/stores/toggleable.ts
@@ -2,9 +2,9 @@ export interface Toggleable<T = void> {
 	toggle(): T;
 }
 
-export function makeToggleable<W, T = void>(o: W, toggler: (o: W) => T): typeof o & Toggleable<T> {
+export function makeToggleable<W, T = void>(object: W, toggler: (object: W) => T): typeof object & Toggleable<T> {
 	return {
-		toggle: () => toggler(o),
-		...o,
+		toggle: () => toggler(object),
+		...object,
 	};
 }
