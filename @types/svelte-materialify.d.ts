e9edc842baf4eda1603e71ed9ea918442c4b68d7
V-ed <guillaumemarcoux@gmail.com>
2021-04-29 15:22:04 -0400

    Add full components types declarations
    This addition allows me to start designing the UI with an annoying - but functionnal - component system that doesn't import everything when not requested
    
    Used in the mean time that rollup / svelte-materialify fixes their setup with tree shaking, or until svelterial is available

--- @types/svelte-materialify.d.ts
+++ @types/svelte-materialify.d.ts
@@ -1,3 +1,158 @@
 declare module 'svelte-materialify/src' {
 	export * from 'svelte-materialify';
 }
+
+declare module 'svelte-materialify/src/components/Alert/Alert.svelte' {
+	export { Alert as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/AppBar/AppBar.svelte' {
+	export { AppBar as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Avatar/Avatar.svelte' {
+	export { Avatar as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Badge/Badge.svelte' {
+	export { Badge as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Breadcrumbs/Breadcrumbs.svelte' {
+	export { Breadcrumbs as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Button/Button.svelte' {
+	export { Button as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Card/Card.svelte' {
+	export { Card as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Card/CardActions.svelte' {
+	export { CardActions as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Card/CardSubtitle.svelte' {
+	export { CardSubtitle as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Card/CardText.svelte' {
+	export { CardText as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Card/CardTitle.svelte' {
+	export { CardTitle as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Checkbox/Checkbox.svelte' {
+	export { Checkbox as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Col/Col.svelte' {
+	export { Col as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Container/Container.svelte' {
+	export { Container as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Dialog/Dialog.svelte' {
+	export { Dialog as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Divider/Divider.svelte' {
+	export { Divider as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/ExpansionPanels/ExpansionPanel.svelte' {
+	export { ExpansionPanel as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/ExpansionPanels/ExpansionPanels.svelte' {
+	export { ExpansionPanels as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Footer/Footer.svelte' {
+	export { Footer as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Icon/Icon.svelte' {
+	export { Icon as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Input/Input.svelte' {
+	export { Input as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/ItemGroup/ItemGroup.svelte' {
+	export { ItemGroup as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/List/List.svelte' {
+	export { List as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/List/ListGroup.svelte' {
+	export { ListGroup as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/List/ListItem.svelte' {
+	export { ListItem as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/List/ListItemGroup.svelte' {
+	export { ListItemGroup as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/MaterialApp/MaterialApp.svelte' {
+	export { MaterialApp as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/MaterialApp/MaterialAppMin.svelte' {
+	export { MaterialAppMin as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Menu/Menu.svelte' {
+	export { Menu as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/NavigationDrawer/NavigationDrawer.svelte' {
+	export { NavigationDrawer as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Overlay/Overlay.svelte' {
+	export { Overlay as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/ProgressCircular/ProgressCircular.svelte' {
+	export { ProgressCircular as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/ProgressLinear/ProgressLinear.svelte' {
+	export { ProgressLinear as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Radio/Radio.svelte' {
+	export { Radio as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Row/Row.svelte' {
+	export { Row as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Select/Select.svelte' {
+	export { Select as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Slider/Slider.svelte' {
+	export { Slider as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Snackbar/Snackbar.svelte' {
+	export { Snackbar as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Subheader/Subheader.svelte' {
+	export { Subheader as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Switch/Switch.svelte' {
+	export { Switch as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Tabs/Tabs.svelte' {
+	export { Tabs as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Tabs/Tab.svelte' {
+	export { Tab as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Tabs/TabContent.svelte' {
+	export { TabContent as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Textarea/Textarea.svelte' {
+	export { Textarea as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/TextField/TextField.svelte' {
+	export { TextField as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/components/Tooltip/Tooltip.svelte' {
+	export { Tooltip as default } from 'svelte-materialify';
+}
+
+declare module 'svelte-materialify/src/actions/ClickOutside' {
+	export { ClickOutside as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/actions/Intersect' {
+	export { Intersect as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/actions/Jump' {
+	export { Jump as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/actions/Ripple' {
+	export { Ripple as default } from 'svelte-materialify';
+}
+declare module 'svelte-materialify/src/actions/Touch' {
+	export { Touch as default } from 'svelte-materialify';
+}
