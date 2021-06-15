40f46115da85b16108a3f95b82674efd315f4fda
V-ed <guillaumemarcoux@gmail.com>
2021-06-15 15:36:04 -0400

    Add brearpoint handling to check for isMobile

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
 	import Navbar from '$/components/nav/Navbar.svelte';
-	import { metatags, page } from '@roxi/routify';
+	import { isMobile, setBreakpoints, themeStore } from '$/stores';
 	import { capitalize } from '$/utils';
-	// import { MaterialAppMin } from 'svelte-materialify/src';
+	import { metatags, page } from '@roxi/routify';
+	import { onMount } from 'svelte';
 	import MaterialAppMin from 'svelte-materialify/src/components/MaterialApp/MaterialAppMin.svelte';
-	import { themeStore } from '$/stores';
 
 	metatags.template('title', (title: string) => `${title ? `${title} - ` : ''}Template`);
 	$: metatags.title = capitalize($page.title);
+
+	onMount(async () => {
+		// @ts-ignore
+		let breakpoints = await import('svelte-materialify/src/utils/breakpoints');
+		setBreakpoints(breakpoints.default);
+		isMobile.refresh();
+	});
 </script>
+
+<svelte:window on:resize={isMobile.refresh} />
 
 <MaterialAppMin theme={$themeStore}>
 	<div class="flex flex-col" style="min-height: 100vh">
